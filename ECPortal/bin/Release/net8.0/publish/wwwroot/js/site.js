5a79180ec5206984681a408e867519ab613da240
Usman-Zeb1 <usman.zeb1@jazz.com.pk>
2024-08-27 11:53:02 +0500

    Delete ECPortal/bin directory

--- ECPortal/bin/Release/net8.0/publish/wwwroot/js/site.js
+++
@@ -1,8 +0,0 @@
-﻿$(document).ready(function () {
-    var today = new Date().toISOString().split('T')[0];
-    $('input[type="date"]').each(function () {
-        if (!$(this).val()) {
-            $(this).val(today);
-        }
-    });
-});
